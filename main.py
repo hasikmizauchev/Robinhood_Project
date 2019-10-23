c426fadd33ba877105f758ce2c8137ed461b1e08
hasikmizauchev <56930097+hasikmizauchev@users.noreply.github.com>
2019-10-23 20:25:02 +0300

    inital commit
    our first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import pandas as pd
+data = pd.read_csv('features_data.csv')
+type(data)
+
+data.info()
+data.describe()
+
+import numpy as np
+t = np.linspace(-6, 6, 20)
+sin_t = np.sin(t)
+cos_t = np.cos(t)
+numbers = pd.DataFrame({'t':t,'sin':sin_t,'cos':cos_t})
+print(numbers)
+
+print(numbers.shape)
+print(numbers.columns)
+print(data.columns)
+print(numbers['t'][0:4])
+
